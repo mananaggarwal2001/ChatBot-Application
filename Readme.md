3dc1637fdeded830a1bceef33c75725971aea724
mananaggarwal2001 <mananaggarwal2001@gmail.com>
2022-07-23 19:21:31 +0530

    Changes Made To Host the Website

--- Readme.md
+++ Readme.md
@@ -4,5 +4,26 @@
 1. **express: ^4.18.1**
 2. **socket.io: ^4.5.1**
 3. **cors: ^2.8.5**
+4. **path:^0.12.7**
 
-##
+## **Steps to Start the Application**
+
+1. Clone This repository by using the command
+
+```
+git clone https://github.com/mananaggarwal2001/ChatBot-Application.git
+```
+2. Then Install the required Packges By Using the command
+
+```
+npm install
+```
+
+3. Then Start the application by using the command
+
+```
+node app.js
+```
+
+## **How To Use the Application**
+### **These Steps are given Sequentially So Please Follow all the Steps Respectively.**
--- app.js
+++ app.js
@@ -1,16 +1,23 @@
 // Node server which will handle the socket io connections
-const io= require('socket.io')(8000); 
+const io= require('socket.io')(8000);
 const users= {};
 const cors= require('cors')
 const express= require('express')
 const app= express();
+const port= 80;
+const path= require('path');
 app.use(cors());
+// For Serving the Static files we have
 
+app.use(express.static(`${__dirname}/public`)) // For Sendingl the Static File
+
+app.get('/', (req,res)=>{
+    res.sendFile(path.join(__dirname, 'index.html'));
+})
 io.on('connection', socket=>{
     // This is the first event that tell the clients which are connected to the server that the new User has joined.
     socket.on('new-user-joined', name=>{
             // This Statement to check which user has joined the chat and what to do with that user.
-        console.log("New User", name);
         users[socket.id]= name;  // This is used to append the array of the names which has joined the chat etc.
         socket.broadcast.emit('user-joined', name); // This is used to tell the others users that this user has joined the chat and ready to do the conversation.
     });
@@ -24,4 +31,10 @@ io.on('connection', socket=>{
         socket.broadcast.emit('left',users[socket.id]);
         delete users[socket.id]
     })
+})
+
+// To Listen the Application and Serve it to the client
+
+app.listen(port, ()=>{
+    console.log("Application is sucessfully listening to the Port 80 and Serving to the client window.");
 })
